41c2f103e0b2f907ac0f9ca561a1a1b9a7a22347
Rob <robbcardenas@gmail.com>
2015-09-03 10:28:07 -0700

    landing page added

---
+++ app/assets/javascripts/sonder.js
@@ -0,0 +1,11 @@
+// center text in middle of the screen
+
+$(document).ready(function(){
+  // $('.welcome').css({ 'width':'100%', 'text-align':'center' });
+  var wel = $('.welcome').height();
+  var h = wel/2;
+  var w1 = $(window).height();
+  var w = w1/2;
+  var m = w - h
+  $('.welcome').css("margin-top",m + "px")
+});
