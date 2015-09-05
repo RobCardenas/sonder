b9cbee41f2d380d64bec7c1c9be1b97e7d2d5cc8
Rob <robbcardenas@gmail.com>
2015-09-04 23:11:34 -0700

    Multiple file upload and cover working. Needs a lot of styling.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150905051042) do
+ActiveRecord::Schema.define(version: 20150905054222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,10 @@ ActiveRecord::Schema.define(version: 20150905051042) do
     t.datetime "updated_at",  null: false
     t.integer  "cover"
     t.string   "token"
+    t.integer  "picture_id"
   end
+
+  add_index "stories", ["picture_id"], name: "index_stories_on_picture_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -52,4 +55,5 @@ ActiveRecord::Schema.define(version: 20150905051042) do
     t.datetime "avatar_updated_at"
   end
 
+  add_foreign_key "stories", "pictures"
 end
