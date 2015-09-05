50b3f0fab3ba42286ee1d5b13096c4697051b42c
Rob <robbcardenas@gmail.com>
2015-09-05 02:32:14 -0700

    multiple file upload working on s3

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,7 +4,14 @@ class Picture < ActiveRecord::Base
 
 	has_attached_file :image,
                    	:styles => { :large => "800x400>", :medium => "400x200>", :thumb => "100x50>" },
-                   	:default_url => "/images/:style/missing.png"
+	                 	:path => "images/:id/:style/image.:extension",
+  									:storage => :s3,
+  									:s3_credentials => Proc.new { |a| a.instance.s3_credentials },
+  									:s3_host_name => 's3.amazonaws.com'
+
+  def s3_credentials
+    { :bucket => ENV['S3_BUCKET'], :access_key_id => ENV['S3_PUBLIC_KEY'], :secret_access_key => ENV['S3_SECRET'] }
+  end
 
  	validates_attachment :image, :presence => true,
                        :content_type => { :content_type => ["image/jpeg", "image/jpg", "image/gif", "image/png"] },
